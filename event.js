5171589e177942a7dd8d0a4f9658743ef5c4bc96
martin <martin@neutronstar.dyndns.org>
2013-04-13 22:07:16 +0200

    Only show ui on google pages.

--- event.js
+++ event.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.init) {
     sendResponse(localStorage.getItem("rules").split("\n"));
+    chrome.pageAction.show(sender.tab.id);
   }
 });
 
